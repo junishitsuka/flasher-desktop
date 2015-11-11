e8c77a8820ac135969e1ed9e19fc627b1402266c
junishitsuka <ishitsuka.jun@gmail.com>
2015-11-11 23:08:32 +0900

    refs #7 ノート編集画面のデザイン調整

--- app/js/src/components/SelectedNote.jsx
+++ app/js/src/components/SelectedNote.jsx
@@ -7,21 +7,21 @@ export default class SelectedNote extends Component {
             <div id="main" className="pure-u-1">
                 <div id="selectednote" className="email-content">
                     <div className="email-content-header pure-g">
-                        <div className="pure-u-1-2">
+                        <div className="pure-u-2-3 pure-form pure-group">
                             <textarea className="email-content-title" onChange={this.props.onTitleChange} value={ note.title } />
                             <p className="email-content-subtitle">
                             at <span>{ note.created_at }</span>
                             </p>
                         </div>
 
-                        <div className="email-content-controls pure-u-1-2">
+                        <div className="email-content-controls pure-u-1-3">
                             <button className="secondary-button pure-button">Star</button>
-                            <button className="secondary-button pure-button">Delete</button>
+                            <button className="secondary-button pure-button">Del</button>
                         </div>
                     </div>
 
-                    <div className="email-content-body">
-                        <textarea onChange={this.props.onDescChange} value={ note.desc } />
+                    <div className="email-content-body pure-form pure-group">
+                        <textarea className="email-content-desc" onChange={this.props.onDescChange} value={ note.desc } />
                     </div>
                 </div>
             </div>
